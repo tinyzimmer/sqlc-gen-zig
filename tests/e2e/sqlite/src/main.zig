b85658d6c756d045275fc4bf5665cb61db0c01cb
Avi Zimmerman <avi.zimmerman@gmail.com>
2025-02-14 11:08:00 +0200

    tests: add unmanaged sqlite tests

--- tests/e2e/sqlite/src/main.zig
+++ tests/e2e/sqlite/src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub const ManagedTests = @import("managed.zig");
+pub const UnmanagedTests = @import("unmanaged.zig");
 
 test {
     std.testing.refAllDecls(@This());
